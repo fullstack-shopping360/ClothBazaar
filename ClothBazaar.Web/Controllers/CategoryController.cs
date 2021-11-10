fb9ad39d9aad6f138d1bd2219ab6048968b3fe4a
VK-vijaya <vijayarani.kuna@gmail.com>
2021-11-10 10:22:55 +0200

    Added Category controller and did coding for all CRUD operations

---
+++ ClothBazaar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,70 @@
+﻿using ClothBazaar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazaar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryServices categoryService = new CategoryServices();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories= categoryService.GetCategory();
+         
+            return View(categories);
+        }
+
+        // GET: Category
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+                
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var category = categoryService.GetCategory(ID);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            
+            categoryService.DeleteCategory(category.ID);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+
+
+}
---
+++ ClothBazar.Services/CategoryServices.cs
@@ -0,0 +1,58 @@
+﻿using ClothBazaar.Database;
+using ClothBazaar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class CategoryServices
+    {
+        public List<Category> GetCategory()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        public Category GetCategory(int id)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.Find(id);
+            }
+        }
+
+        public void SaveCategory(Category category)
+        {
+            using(var context=new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(int id)
+        {
+            using (var context = new CBContext())
+            {
+                //context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+                var category = context.Categories.Find(id);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
